84677efbdca18954563078813b34daeea7f0d5fd
Matiusco <matius.couto@gmail.com>
2021-10-05 11:33:37 -0300

    opening comments
    Just a few statements and thanks. It also suggests people's involvements to collaborate.

--- config-prompt.sh
+++ config-prompt.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+# <https://github.com/Matiusco/ps1-bash/>
+# coding	: utf-8
+# 
+# support	: 
+# LICENSE	: GNU General Public License v3.0
+# VERSION	:  
+#-----------------------------------------------------------------------
+# This script is inspired by the beautiful work done 
+# by Paulo Kretcheu <https://github.com/kretcheu/devel/>
+#-----------------------------------------------------------------------
+# Many code examples were adapted from Teacher BlauAraujo's bash course
+# <https://debxp.org/cbpb//>
+#-----------------------------------------------------------------------
+# You are encouraged to collaborate in translate into English 
+# or other languages. Contact-me here in github.
+# New prompt color suggestions can be sent in issue.
+#-----------------------------------------------------------------------
